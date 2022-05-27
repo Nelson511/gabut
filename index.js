ab1fe7bbf82ac2929cf9f0f0bb187903652a438b
Nelson511 <106333871+Nelson511@users.noreply.github.com>
2022-05-27 15:05:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1085,7 +1085,7 @@ case 'ngewe': {
 				if (!q) return reply(`Penggunaan ${command} text\n\nContoh : ${command} saya menjadi wibu`)
 					const apa = ['Iya', 'Tidak', 'Bisa Jadi', 'Mungkin','ya nda tau ko tanya saya']
 					const kah = apa[Math.floor(Math.random() * apa.length)]
-chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : Apakah ${q}\n│\n│Jawaban : ${kah}\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : Apakah ${q}\n│\n│Jawaban : ${kah}\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 					break
 
 
@@ -1094,21 +1094,21 @@ case 'bisakah':
 				if (!q) return reply(`Penggunaan ${command} text\n\nContoh : ${command} saya wibu`)
 					const bisa = ['Bisa','Gak Bisa','mustahil','TENTU PASTI KAMU BISA!!!!']
 					const ga = bisa[Math.floor(Math.random() * bisa.length)]
-chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : ${q}\n│\n│Jawaban : ${ga}\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : ${q}\n│\n│Jawaban : ${ga}\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 
 					break
 case 'bagaimanakah':
 				if (!q) return reply(`Penggunaan ${command} text\n\nContoh : ${command} saya wibu`)
 					const gimana = ['Gak Gimana2', 'Sulit Itu Bro', 'Maaf Bot Tidak Bisa Menjawab', 'Coba Deh Cari Di Gugel','astaghfirallah Beneran???','Pusing ah','Owhh Begitu:(','Yang Sabar Ya Bos:(','Gimana yeee']
 					const ya = gimana[Math.floor(Math.random() * gimana.length)]
-chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : ${q}\n│\n│Jawaban : ${ya}\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : ${q}\n│\n│Jawaban : ${ya}\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 					break
 
                     case 'rate':
 				    if (!q) return reply(`Penggunaan ${command} text\n\nContoh : ${command} Gambar aku`)
 					const ra = ['5', '10', '15' ,'20', '25','30','35','40','45','50','55','60','65','70','75','80','85','90','100']
 					const te = ra[Math.floor(Math.random() * ra.length)]
-                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Rate : ${q}\n│\n│Jawaban : *${te}%*\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Rate : ${q}\n│\n│Jawaban : *${te}%*\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 					break
 
 case 'gantengcek':
@@ -1116,7 +1116,7 @@ case 'cekganteng':
 				    if (!q) return reply(`Penggunaan ${command} Nama\n\nContoh : ${command} Nelson`)
 					const gan = ['5', '10', '15' ,'20', '25','30','35','40','45','50','55','60','65','70','75','80','85','90','100']
 					const teng = gan[Math.floor(Math.random() * gan.length)]
-                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Nama : ${q}\n│\n│Jawaban : *${teng}%*\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Nama : ${q}\n│\n│Jawaban : *${teng}%*\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 					break
 
 case 'cantikcek':
@@ -1124,7 +1124,7 @@ case 'cekcantik':
 				    if (!q) return reply(`Yang Mau di Cek apaan Titit ?`)
 					const can = ['5', '10', '15' ,'20', '25','30','35','40','45','50','55','60','65','70','75','80','85','90','100']
 					const tik = can[Math.floor(Math.random() * can.length)]
-                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Nama : ${q}\n│\n│Jawaban : *${tik}%*\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Nama : ${q}\n│\n│Jawaban : *${tik}%*\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 					break
 
 case 'sangecek':
@@ -1136,14 +1136,14 @@ case 'ceklesbi':
 					if (!q) return reply(`Penggunaan ${command} Nama\n\nContoh : ${command} ${pushname}`)
 					const sangeh = ['5', '10', '15','20', '25','30','35','40','45','50','55','60','65','70','75','80','85','90','100']
 					const sange = sangeh[Math.floor(Math.random() * sangeh.length)]
-                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Nama : ${q}\n│\n│Jawaban : *${sange}%*\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Nama : ${q}\n│\n│Jawaban : *${sange}%*\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 					break
 
 case '.kapankah':
 				    if (!q) return reply(`Penggunaan ${command} Pertanyaan\n\nContoh : ${command} Saya Mati`)
 					const kapan = ['5 Hari Lagi', '10 Hari Lagi', '15 Hari Lagi','20 Hari Lagi', '25 Hari Lagi','30 Hari Lagi','35 Hari Lagi','40 Hari Lagi','45 Hari Lagi','50 Hari Lagi','55 Hari Lagi','60 Hari Lagi','65 Hari Lagi','70 Hari Lagi','75 Hari Lagi','80 Hari Lagi','85 Hari Lagi','90 Hari Lagi','100 Hari Lagi','5 Bulan Lagi', '10 Bulan Lagi', '15 Bulan Lagi','20 Bulan Lagi', '25 Bulan Lagi','30 Bulan Lagi','35 Bulan Lagi','40 Bulan Lagi','45 Bulan Lagi','50 Bulan Lagi','55 Bulan Lagi','60 Bulan Lagi','65 Bulan Lagi','70 Bulan Lagi','75 Bulan Lagi','80 Bulan Lagi','85 Bulan Lagi','90 Bulan Lagi','100 Bulan Lagi','1 Tahun Lagi','2 Tahun Lagi','3 Tahun Lagi','4 Tahun Lagi','5 Tahun Lagi','Besok','Lusa',`Abis Command Ini Juga Lu ${q}`]
 					const kapankah = kapan[Math.floor(Math.random() * kapan.length)]
-                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : ${q}\n│\n│Jawaban : *${kapankah}*\n╰────\n\n 𝙉𝙚𝙡𝘽𝙤𝙩-𝐌𝐃` }, { quoted: m })
+                    chika.sendMessage(from, { text: `╭─「 *Semoga Beruntung* 」\n│\n│Pertanyaan : ${q}\n│\n│Jawaban : *${kapankah}*\n╰────\n\n FangzBOTZ | FangzGanz` }, { quoted: m })
 					break
 
 case 'wangy':
